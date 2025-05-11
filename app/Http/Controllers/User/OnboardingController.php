5885de6a6625379e79bd3ec234b0167106b98e4b
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-05-12 02:19:08 +0600

    Update OnboardingController.php

--- app/Http/Controllers/User/OnboardingController.php
+++ app/Http/Controllers/User/OnboardingController.php
@@ -4,15 +4,22 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class OnboardingController extends Controller
 {
     public function index()
     {
         // return redirect()->route('user.maintenance');
-        if (user()) {
-            return redirect()->route('user.home');
+
+        if (Auth::check()) {
+            Auth::logout();
+
+            // Clear session data
+            request()->session()->invalidate();
+            request()->session()->regenerateToken();
         }
+
         return view('auth.onboarding');
     }
 
