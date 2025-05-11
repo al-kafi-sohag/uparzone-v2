da690abe62f79f28ad1646ac5f70483b4394f653
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-05-12 03:15:38 +0600

    reverting login logic

--- app/Http/Controllers/User/OnboardingController.php
+++ app/Http/Controllers/User/OnboardingController.php
@@ -4,22 +4,15 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class OnboardingController extends Controller
 {
     public function index()
     {
         // return redirect()->route('user.maintenance');
-
-        if (Auth::check()) {
-            Auth::logout();
-
-            // Clear session data
-            request()->session()->invalidate();
-            request()->session()->regenerateToken();
+        if (user()) {
+            return redirect()->route('user.home');
         }
-
         return view('auth.onboarding');
     }
 
