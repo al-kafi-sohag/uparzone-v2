1381b51e7640561458940e8aadfbee2f7ec1b741
al-kafi-sohag <85807115+al-kafi-sohag@users.noreply.github.com>
2025-05-14 02:03:46 +0600

    Update wallet.js

--- public/user/js/wallet.js
+++ public/user/js/wallet.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $('#withdrawal-form').submit(function(e) {
         const $form = $(this);
-        $form.find('#main-container').css('display', 'none');
+        $form.find('#main-container').css('opacity', '0');
         $form.find('#withdrawal-form-container').append('<div class="flex justify-center items-center py-10"><div class="animate-spin rounded-full h-8 w-8 border-b-2 border-teal-500"></div></div>');
         console.log('Withdrawal form submitted');
     });
